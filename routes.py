06c56d321d8de7074b33860ac81fb9cfde9d2907
Itay Roll <itroll@microsoft.com>
2018-05-14 21:28:34 +0300

    First attempt sum

--- routes.py
+++ routes.py
@@ -1,35 +1,27 @@
-"""
-Routes and views for the bottle application.
-"""
+from bottle import route, view, run, request
+import telegram
 
-from bottle import route, view
-from datetime import datetime
+TOKEN = '552988587:AAFOqPcC-5eDV3HWeYS9yUGGsx_2IiLLCxM'
+APPNAME = 'worldcupbetsbot'
 
-@route('/')
-@route('/home')
-@view('index')
-def home():
-    """Renders the home page."""
-    return dict(
-        year=datetime.now().year
-    )
+@route('/setWebhook')
+def setWebhook():
+    bot = telegram.Bot(TOKEN)
+    botWebhookResult = bot.setWebhook(webhook_url='https://{}.azurewebsites.net/botHook'.format(APPNAME))
+    return str(botWebhookResult)
 
-@route('/contact')
-@view('contact')
-def contact():
-    """Renders the contact page."""
-    return dict(
-        title='Contact',
-        message='Your contact page.',
-        year=datetime.now().year
-    )
+@route('/botHook', method='POST')
+def botHook():
+    bot = telegram.Bot(TOKEN)
+    update = telegram.update.Update.de_json(request.json, bot)
+    bot.sendMessage(chat_id=update.message.chat_id, text=getSum(update.message.text, update.message.from_user.username))
+    return 'OK'
 
-@route('/about')
-@view('about')
-def about():
-    """Renders the about page."""
-    return dict(
-        title='About',
-        message='Your application description page.',
-        year=datetime.now().year
-    )
+def getSum(query, userName):
+    try:
+        splittedBySum = query.split('+')
+        if len(splittedBySum) != 2:
+            raise ValueError('Too complicated stuff')
+        return str(int(splittedBySum[0]) + int(splittedBySum[1]))
+    except:
+        return  "I'm sorry, {}. I'm afraid I can't do that".format(userName)
