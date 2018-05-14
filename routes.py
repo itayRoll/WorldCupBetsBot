ca20792f528d90c4e1e4af89ad6f0955e8cf1719
Itay Roll <itroll@microsoft.com>
2018-05-14 22:43:19 +0300

    Support inline query vol.2

--- routes.py
+++ routes.py
@@ -15,7 +15,14 @@ def botHook():
 	bot = telegram.Bot(TOKEN)
 	update = telegram.update.Update.de_json(request.json, bot)
 	if update.message is not None:
-		bot.sendMessage(chat_id=update.message.chat_id, text='Got you message loud and clear, {}.'.format(update.message.from_user.username))
+		bot.sendMessage(chat_id=update.message.chat_id, text='Got your message loud and clear, {}.'.format(update.message.from_user.username))
 	elif update.inline_query is not None:
-		bot.answerInlineQuery(update.inline_query.id, ['First option - which is nice', 'Second option - not as much', 'Third option - total shit'])
+		res = []
+		res.append(getArticleFromText('First option - which is nice'))
+		res.append(getArticleFromText('Second option - not as much'))
+		res.append(getArticleFromText('Third option - total shit'))
+		bot.answerInlineQuery(update.inline_query.id, res)
 	return 'OK'
+
+def getArticleFromText(text):
+	return telegram.InlineQueryResultArticle(id=text.upper(), title=text, input_message_content=telegram.InputTextMessageContent(text))
