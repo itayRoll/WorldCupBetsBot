0805ca2548c32ef4c0d77b615147f6c64ba94867
Itay Roll <itroll@microsoft.com>
2018-05-14 22:26:37 +0300

    Support inline query

--- routes.py
+++ routes.py
@@ -14,14 +14,8 @@ def setWebhook():
 def botHook():
 	bot = telegram.Bot(TOKEN)
 	update = telegram.update.Update.de_json(request.json, bot)
-	bot.sendMessage(chat_id=update.message.chat_id, text=getSum(update.message.text, update.message.from_user.username))
+	if update.message is not None:
+		bot.sendMessage(chat_id=update.message.chat_id, text='Got you message loud and clear, {}.'.format(update.message.from_user.username))
+	elif update.inline_query is not None:
+		bot.answerInlineQuery(update.inline_query.id, ['First option - which is nice', 'Second option - not as much', 'Third option - total shit'])
 	return 'OK'
-
-def getSum(query, userName):
-	try:
-		splittedBySum = query.split('+')
-		if len(splittedBySum) != 2:
-			raise ValueError('Too complicated stuff')
-		return str(int(splittedBySum[0]) + int(splittedBySum[1]))
-	except:
-		return  "I'm sorry, {}. I'm afraid I can't do that".format(userName)
