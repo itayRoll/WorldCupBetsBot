c19cea7ef96c5f2b7994880286d6fa993937f2a4
Itay Roll <itroll@microsoft.com>
2018-05-14 21:57:25 +0300

    Replace leading spaces with tabs

--- app.py
+++ app.py
@@ -10,29 +10,29 @@ import sys
 import routes
 
 class SSLWSGIRefServer(bottle.ServerAdapter):
-    def run(self, handler):
-        from wsgiref.simple_server import make_server, WSGIRequestHandler
-        import ssl
-        if self.quiet:
-            class QuietHandler(WSGIRequestHandler):
-                def log_request(*args, **kw): pass
-            self.options['handler_class'] = QuietHandler
-        srv = make_server(self.host, self.port, handler, **self.options)
-        srv.socket = ssl.wrap_socket (srv.socket,
-        certfile='server.pem', server_side=True)
-        srv.serve_forever()
+	def run(self, handler):
+		from wsgiref.simple_server import make_server, WSGIRequestHandler
+		import ssl
+		if self.quiet:
+			class QuietHandler(WSGIRequestHandler):
+				def log_request(*args, **kw): pass
+			self.options['handler_class'] = QuietHandler
+		srv = make_server(self.host, self.port, handler, **self.options)
+		srv.socket = ssl.wrap_socket (srv.socket,
+		certfile='server.pem', server_side=True)
+		srv.serve_forever()
 
 if '--debug' in sys.argv[1:] or 'SERVER_DEBUG' in os.environ:
-    # Debug mode will enable more verbose output in the console window.
-    # It must be set at the beginning of the script.
-    bottle.debug(True)
+	# Debug mode will enable more verbose output in the console window.
+	# It must be set at the beginning of the script.
+	bottle.debug(True)
 
 def wsgi_app():
-    """Returns the application to make available through wfastcgi. This is used
-    when the site is published to Microsoft Azure."""
-    return bottle.default_app()
+	"""Returns the application to make available through wfastcgi. This is used
+	when the site is published to Microsoft Azure."""
+	return bottle.default_app()
 
 if __name__ == '__main__':
 	app = bottle.Bottle()
-    srv = SSLWSGIRefServer(host="worldcupbetsbot.azurewebsites.net", port=80)
-    bottle.run(server=srv)
+	srv = SSLWSGIRefServer(host="worldcupbetsbot.azurewebsites.net", port=80)
+	bottle.run(server=srv)
--- routes.py
+++ routes.py
@@ -6,22 +6,22 @@ APPNAME = 'worldcupbetsbot'
 
 @route('/setWebhook')
 def setWebhook():
-    bot = telegram.Bot(TOKEN)
-    botWebhookResult = bot.setWebhook(webhook_url='https://{}.azurewebsites.net/botHook'.format(APPNAME))
-    return str(botWebhookResult)
+	bot = telegram.Bot(TOKEN)
+	botWebhookResult = bot.setWebhook(webhook_url='https://{}.azurewebsites.net/botHook'.format(APPNAME))
+	return str(botWebhookResult)
 
 @route('/botHook', method='POST')
 def botHook():
-    bot = telegram.Bot(TOKEN)
-    update = telegram.update.Update.de_json(request.json, bot)
-    bot.sendMessage(chat_id=update.message.chat_id, text=getSum(update.message.text, update.message.from_user.username))
-    return 'OK'
+	bot = telegram.Bot(TOKEN)
+	update = telegram.update.Update.de_json(request.json, bot)
+	bot.sendMessage(chat_id=update.message.chat_id, text=getSum(update.message.text, update.message.from_user.username))
+	return 'OK'
 
 def getSum(query, userName):
-    try:
-        splittedBySum = query.split('+')
-        if len(splittedBySum) != 2:
-            raise ValueError('Too complicated stuff')
-        return str(int(splittedBySum[0]) + int(splittedBySum[1]))
-    except:
-        return  "I'm sorry, {}. I'm afraid I can't do that".format(userName)
+	try:
+		splittedBySum = query.split('+')
+		if len(splittedBySum) != 2:
+			raise ValueError('Too complicated stuff')
+		return str(int(splittedBySum[0]) + int(splittedBySum[1]))
+	except:
+		return  "I'm sorry, {}. I'm afraid I can't do that".format(userName)
